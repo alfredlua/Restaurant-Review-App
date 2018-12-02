0c38d4294f30bc4ea0c41958b0165b8fc892ef5c
Alfred <alfred@bufferapp.com>
2018-12-02 13:24:46 +0800

    fix: Add ignoreSearch for cache

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
 
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request, {ignoreSearch: true}).then(function(response) {
       return response || fetch(event.request);
     })
   );
